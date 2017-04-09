737bde8dba13e057985c174356bbc09a184f18e4
roni <roni.andrid@gmail.com>
2017-04-09 15:12:10 +0600

    nothing change

---
+++ app/src/main/java/joyit/pencil/roni/sodaikhana/lanching.java
@@ -0,0 +1,17 @@
+package joyit.pencil.roni.sodaikhana;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class lanching extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lanching);
+    }
+
+}
